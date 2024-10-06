e52f8cd98fe7864c5c1c8c8634c0a76c50a2126e
siddharth nandagopal <snandagopal@ibasis.net>
2024-10-06 17:41:41 -0400

    parallelize web scraper with Ray to speed up

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # web-scraper
-a simple web scraper in Python
+a simple web scraper in Python & parallelize it with Ray to speed up
 
 ## steps to run
 1. install dependencies
@@ -24,6 +24,20 @@ a simple web scraper in Python
     sample output:
     ```
     begin web-scraper...
+    INFO worker.py:1786 -- Started a local Ray instance.
     number of links scraped from base https://docs.ray.io/en/latest/index.html till depth 2: 288
-    python3 main.py  11.48s user 0.24s system 25% cpu 46.391 total
+    python3 main.py  4.80s user 2.83s system 10% cpu 1:14.73 total
+    ```
+
+    ```
+    begin web-scraper...
+    INFO worker.py:1786 -- Started a local Ray instance.
+    web-scraper parallely run on 6 links
+    339
+    339
+    339
+    200
+    200
+    200
+    python3 main.py  4.00s user 2.22s system 10% cpu 58.924 total
     ```
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from find_links import find_links
+import ray
+
+@ray.remote
+def find_links_task(start_url, base_url, depth=2):
+    return find_links(start_url, base_url, depth)
 
 print(f"begin web-scraper...")
 
@@ -6,5 +11,11 @@ base = "https://docs.ray.io/en/latest/"
 docs = base + "index.html"
 depth = 2
 
-links = find_links(docs, base, depth)
-print(f"number of links scraped from base {docs} till depth {depth}: {len(links)}")
+# run six crawlers in parallel, the first three (redundantly) crawl docs.ray.io again, 
+# the other three crawl the main entry points of the Ray RLlib, Tune, and Serve libraries, respectively
+links = [find_links_task.remote(f"{base}{lib}/index.html", base)
+         for lib in ["", "", "", "rllib", "tune", "serve"]]
+
+print(f"web-scraper parallely run on 6 links")
+
+for res in ray.get(links): print(f"{len(res)}")
